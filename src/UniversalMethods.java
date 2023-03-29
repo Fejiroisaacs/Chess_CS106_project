4e77ef52d5fb1e805d3deb7be4e1067e2faac7aa
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2023-03-29 16:18:04 -0400

    updated the universalMethods to use switches instead of if statements, making it simplistic. Also added a method to switch letter to coordinates.

--- src/UniversalMethods.java
+++ src/UniversalMethods.java
@@ -1,27 +1,32 @@
 public class UniversalMethods {
     public static String changeCord(int xPos){
-        if(xPos == 1){
-            return "a";
-        } else if(xPos == 2){
-            return "b";
-        } else if (xPos == 3) {
-            return "c";
-        }else if (xPos == 4) {
-            return "d";
-        }else if (xPos == 5) {
-            return "e";
-        }else if (xPos == 6) {
-            return "f";
-        }else if (xPos == 7) {
-            return "g";
-        }else if (xPos == 8) {
-            return "h";
-        } else {
-            throw new IllegalArgumentException("Wrong X-Coordinate");
+        switch (xPos){
+            case 1 : return "a";
+            case 2 : return "b";
+            case 3 : return "c";
+            case 4 : return "d";
+            case 5 : return "e";
+            case 6 : return "f";
+            case 7 : return "g";
+            case 8 : return "h";
         }
+        throw new IllegalArgumentException("Wrong X-Coordinate, must be between 1-8 inclusive");
     }
     public static String changeColor(String color){
         if(color.equals("Black")) return "\u001B[47m" + "\u001B[30m";
         return "\u001B[47m";
+    }
+    public static int changeLetCord(String cord){
+        switch (cord){
+            case "a": return 1;
+            case "b": return 2;
+            case "c": return 3;
+            case "d": return 4;
+            case "e": return 5;
+            case "f": return 6;
+            case "g": return 7;
+            case "h": return 8;
+        }
+        throw new IllegalArgumentException("the coordinate entered is invalid must be between a-h");
     }
 }
