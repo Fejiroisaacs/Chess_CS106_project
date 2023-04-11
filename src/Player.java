58efe0961a396678e7f095d234b064bdc4b8651f
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2023-04-11 12:29:12 -0400

    completed the player class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,17 @@
 public class Player {
+    private final String color;
+    private boolean move;
+
+    public Player(String color) {
+        this.color = color;
+    }
+    public void setMove(boolean move){
+        this.move = move;
+    }
+    public boolean getMove(){
+        return this.move;
+    }
+    public String getColor() {
+        return this.color;
+    }
 }
