78b5512c6dc9cbe7ea365fd899712388e1375d3e
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2023-04-12 01:02:07 -0400

    updated rook class, rook can move and capture. Ran tests for rook movements and captures

--- src/Main.java
+++ src/Main.java
@@ -23,10 +23,61 @@ public class Main {
 
         thePawn.move(1,5, chessBoard);
         System.out.println(chessBoard);
-        otherPawn.move(1,4, chessBoard);
-        System.out.println(chessBoard);
         // end of pawn tests
 
+        //rook move tests
+        //move up
+        Piece thisRook = chessBoard.getBoard()[0][0];
+        System.out.println(thisRook);
+        System.out.println(thisRook.getXPos() + " " + thisRook.getYPos());
+        thisRook.move(1,3, chessBoard);
+        System.out.println(chessBoard);
+
+        //move right
+        thisRook.move(8,3, chessBoard);
+        System.out.println(chessBoard);
+
+        //move left
+        thisRook.move(5,3, chessBoard);
+        System.out.println(chessBoard);
+
+        //move left
+        thisRook.move(1,3, chessBoard);
+        System.out.println(chessBoard);
+
+        //move down
+        thisRook.move(1,1, chessBoard);
+        System.out.println(chessBoard);
+
+        //illegal move test
+        thisRook.move(25,3, chessBoard);
+        System.out.println(chessBoard);
+
+        // rook capture tests
+        // rook up
+        thisRook.move(1,3, chessBoard);
+        System.out.println(chessBoard);
+
+        //move right
+        thisRook.move(5,3, chessBoard);
+        System.out.println(chessBoard);
+
+        // rook up
+        thisRook.move(5,5, chessBoard);
+        System.out.println(chessBoard);
+
+        thisRook.move(2,5, chessBoard);
+        System.out.println(chessBoard);
+        // rook capture
+        thisRook.canCapture(1,5, chessBoard);
+        System.out.println(chessBoard);
+        // rook capture
+        thisRook.canCapture(1,8, chessBoard);
+        System.out.println(chessBoard);
+        // rook capture
+        thisRook.canCapture(2,8, chessBoard);
+        System.out.println(chessBoard);
+        // end of rook tests
     }
 }
 
--- src/Rook.java
+++ src/Rook.java
@@ -3,6 +3,7 @@ public class Rook implements Piece {
     private int yPos;
     private final String color;
 
+
     Rook(int xPos, int yPos, String color){
         this.color = color;
         this.xPos = xPos;
@@ -17,18 +18,59 @@ public class Rook implements Piece {
 
     @Override
     public boolean canMove(int xPos, int yPos, Board board) {
-        return false;
+        assert (Math.abs(this.xPos - xPos) > 0 || Math.abs(this.yPos - yPos) > 0) ;
+        assert (Math.abs(this.xPos - xPos) < 8 && Math.abs(this.yPos - yPos) < 8);
+        int hold = this.xPos;
+
+        if (xPos > this.xPos) {
+            while(hold < xPos-1){
+                hold++;
+                if(board.getBoard()[this.yPos-1][hold-1] != null) return false;
+            }
+        } else if (xPos < this.xPos) {
+            System.out.println("i got here");
+            while(hold > xPos+1){
+                hold--;
+                if(board.getBoard()[this.yPos-1][hold-1] != null) return false;
+            }
+        } else if (yPos > this.yPos) {
+            hold = this.yPos;
+            while(hold < yPos-1){
+                hold++;
+                if(board.getBoard()[hold-1][this.xPos-1] != null) return false;
+            }
+        } else if (yPos < this.yPos) {
+            hold = this.yPos;
+            while(hold > yPos+1){
+                hold--;
+                if(board.getBoard()[hold-1][this.xPos-1] != null) return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public void move(int xPos, int yPos, Board board){
-        board.editBoard(this, this.xPos, this.yPos, xPos, yPos);
-        this.yPos = yPos;
-        this.xPos = xPos;
+
+        if(canMove(xPos, yPos, board) && board.getBoard()[yPos-1][xPos-1] == null) {
+            board.editBoard(this, this.xPos, this.yPos, xPos, yPos);
+            this.yPos = yPos;
+            this.xPos = xPos;
+        } else System.out.println("Can't move there");
+        //throw new IllegalArgumentException("Can't move there");
+
     }
 
     @Override
     public boolean canCapture(int xPos, int yPos, Board board) {
+        Piece currPiece = board.getBoard()[yPos-1][xPos-1];
+        if(canMove(xPos,yPos,board)  && currPiece != null && !currPiece.getColor().equals(this.color)){
+            board.editBoard(this, this.xPos, this.yPos, xPos, yPos);
+            this.yPos = yPos;
+            this.xPos = xPos;
+            return true;
+        } else System.out.println("Can't capture there");
+        //throw new IllegalArgumentException("Can't capture there");
         return false;
     }
 
