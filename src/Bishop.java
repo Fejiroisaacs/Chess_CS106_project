79cc78fd22995f6717e343d999dbc033a72f600c
Ahmed Haj Ahmed <ahajahmed@haverford.edu>
2023-05-02 15:49:56 -0400

    addded comments to Bishop class

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,13 +1,14 @@
 public class Bishop implements Piece{
-    private int xPos;
-    private int yPos;
-    private final String color;
+    private int xPos;                       // The x-position of the Bishop on the board
+    private int yPos;                       // The y-position of the Bishop on the board
+    private final String color;             // The color of the Bishop
+
 
     /**
-     *
-     * @param xPos the bishops position on  a-f scale
-     * @param yPos the bishops position on the 1-8 scale
-     * @param color the bishops color
+     * This is a constructor to create a Bishop with the specified x-position, y-position, and color
+     * @param xPos the x-position of the Bishop on the board (a-h scale)
+     * @param yPos the y-position of the Bishop on the board (1-8 scale)
+     * @param color the color of the Bishop ("Black" or "White")
      */
     public Bishop(int xPos, int yPos, String color) {
         this.xPos = xPos;
@@ -15,22 +16,25 @@ public class Bishop implements Piece{
         this.color = color;
     }
 
+
     /**
-     *
-     * @return the a-h position of the bishop ( in coordinate form )
+     * This method returns the x-position of the Bishop
+     * @return the x-position of the Bishop on the board (a-h scale)
      */
     @Override
     public int getXPos() { return this.xPos; }
 
+
     /**
-     *
-     * @return the 1-8 position of the bishop
+     * This method returns the y-position of the Bishop
+     * @return the y-position of the Bishop on the board (1-8 scale)
      */
     @Override
     public int getYPos() { return this.yPos; }
 
+
     /**
-     *
+     * This method checks if the Bishop can move to the specified coordinates on the board
      * @param xPos the a-h position the bishop wants to move to
      * @param yPos the 1-8 position the bishop wants to move to
      * @param board the chessboard
@@ -40,18 +44,18 @@ public class Bishop implements Piece{
     public boolean canMove(int xPos, int yPos, Board board) {
         // uses a universal method because queen also moves like a bishop
         return UniversalMethods.canMoveBishop(this.xPos, this.yPos, xPos, yPos, board);
-
     }
 
+
     /**
-     *
-     * @param xPos the a-h position the bishop wants to move to
-     * @param yPos the 1-8 position the bishop wants to move to
+     * This method moves the Bishop to the specified coordinates on the board
+     * @param xPos the x-position (scale a-h) the bishop wants to move to
+     * @param yPos the y-position (scale 1-8) the bishop wants to move to
      * @param board the chessboard
      */
     @Override
     public void move(int xPos, int yPos, Board board) {
-        // checks if the coordinate the bishop wants to move to has a piece there
+        // checks if coordinate the bishop wants to move to has a piece there
         // checks if the bishop can move to that position
         if(canMove(xPos, yPos, board) && board.getBoard()[yPos-1][xPos-1] == null) {
             board.editBoard(this, this.xPos, this.yPos, xPos, yPos);
@@ -62,10 +66,11 @@ public class Bishop implements Piece{
         //throw new IllegalArgumentException("Can't move there")
     }
 
+
     /**
-     *
-     * @param xPos the a-h position the bishop wants to move to and capture
-     * @param yPos the 1-8 position the bishop wants to move to and capture
+     * This method checks if the Bishop can capture a piece at the specified coordinates on the board
+     * @param xPos the x-position the bishop wants to move to and capture
+     * @param yPos the y-position the bishop wants to move to and capture
      * @param board the chessboard
      */
     @Override
@@ -74,6 +79,7 @@ public class Bishop implements Piece{
             System.out.println("Can't capture off the board");
             return false;
         }
+
         Piece currPiece = board.getBoard()[yPos-1][xPos-1];
 
         // checks if there's a piece where the bishop wants to capture
@@ -88,28 +94,43 @@ public class Bishop implements Piece{
         return false;
     }
 
+
     /**
-     *
-     * @param piece the piece we might be attacking
+     * This method determines if this bishop is attacking a given piece on the given chessboard
+     * @param piece the piece we are attacking
      * @param board the chessboard
-     * @return if this(the bishop) is attacking that piece or not
+     * @return true if this (the bishop) is attacking the given piece, false otherwise
      */
     @Override
     public boolean isAttacking(Piece piece, Board board) {
         return this.canMove(piece.getXPos(), piece.getYPos(), board);
     }
 
+
     /**
-     *
+     * This method return the color of the Bishop
      * @return the color of the bishop "Black" or "White"
      */
     @Override
-    public String getColor() { return this.color; }
+    public String getColor() {
+        return this.color;
+    }
+
 
     /**
-     *
+     * This method returns a unique display of this bishop, including its color
+     * and position on the board
      * @return a unique display of the bishop with its color and its position
      */
     @Override
-    public String toString(){ return UniversalMethods.changeColor(this.color) + "Bishp " + UniversalMethods.changeCord(xPos) + yPos + "\u001B[0m"; }
+    public String toString() {
+        return UniversalMethods.changeColor(this.color) +
+                "Bishp " +
+                UniversalMethods.changeCord(xPos) + yPos + "\u001B[0m";
+    }
+
+
 }
+
+
+
