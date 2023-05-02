49db4bbe3d6222f0f34cbf1b169841b47a827613
Ahmed Haj Ahmed <ahajahmed@haverford.edu>
2023-05-02 14:36:16 -0400

    added comments in Player class

--- src/Player.java
+++ src/Player.java
@@ -1,30 +1,63 @@
 import java.util.ArrayList;
 
 public class Player {
-    private final String color;
-    private boolean myTurn=false;
-    private final Board board;
-    private final ArrayList<Piece> myPieces = new ArrayList<>();
+    private final String color;             // color of player, either "White" or "Black"
+    private boolean myTurn = false;         // boolean indicating whether it is currently the player's turn
+    private final Board board;              // the chess board associated with the player
+    private final ArrayList<Piece> myPieces = new ArrayList<>();        // the pieces owned by the player
+    private boolean isMated;                // boolean indicating whether the player is checkmated
 
-    private boolean isMated;
 
+    /**
+     * This is a constructs for new player with the given color and board.
+     * @param color the color of the player, either "White" or "Black"
+     * @param board the chess board associated with the player
+     */
     public Player(String color, Board board) {
         this.color = color;
         this.board = board;
         if(color.equals("White")) myTurn = true;
     }
-    public boolean getIsMated(){
+
+
+    /**
+     * This method returns whether the player is checkmated or not
+     * @return true if the player is checkmated, false otherwise
+     */
+    public boolean getIsMated() {
         return isMated;
     }
-    public void setMated(){
+
+
+    /**
+     * This method sets the player's checkmated boolean to true
+     */
+    public void setMated() {
         this.isMated = true;
     }
-    public void setMyTurn(boolean myTurn){
+
+
+    /**
+     * This method sets the boolean indicating whether it is currently the player's turn or not
+     * @param myTurn true if it is the player's turn, false otherwise
+     */
+    public void setMyTurn(boolean myTurn) {
         this.myTurn = myTurn;
     }
+
+
+    /**
+     * This method returns whether it is currently the player's turn or not
+     * @return true if it is the player's turn, false otherwise
+     */
     public boolean getTurn(){
         return this.myTurn;
     }
+
+
+    /**
+     * This method updates the player's list of owned pieces based on the current state of the board
+     */
     public void updatePieces() {
         for (int i = 7; i >= 0; i--) {
             for (int j = 0; j < 8; j++) {
@@ -35,7 +68,13 @@ public class Player {
             }
         }
     }
-    public King getMyking(){
+
+
+    /**
+     * This method returns the king owned by the player
+     * @return the king owned by the player
+     */
+    public King getMyking() {
         updatePieces();
         King king = null;
         for(Piece currPiece: myPieces){
@@ -44,20 +83,43 @@ public class Player {
                 break;
             }
         }
+
         assert king != null;
         return king;
     }
 
-    public boolean isCheckmated(Piece piece){
+
+    /**
+     * This method returns whether the player is checkmated by the given attacking piece or not
+     * @param piece the attacking piece
+     * @return true if the player is checkmated, false otherwise
+     */
+    public boolean isCheckmated(Piece piece) {
         King king = getMyking();
         return piece.isAttacking(king, board) && king.checkmated(board);
-
     }
 
+
+    /**
+     * This method determines whether the player is in check or not
+     * @param piece the piece to check
+     * @return true if the player is in check, false otherwise
+     */
     public boolean isChecked(Piece piece){
         return piece.isAttacking(getMyking(), board);
     }
+
+
+    /**
+     * This method returns the color of the player
+     * @return the color of the player
+     */
     public String getColor() {
         return this.color;
     }
+
+
 }
+
+
+
