32da71298d66bc44084a90192d427f47f478c3cb
Ahmed Haj Ahmed <ahajahmed@haverford.edu>
2023-05-02 22:25:43 -0400

    added comments to Queen class

--- src/Queen.java
+++ src/Queen.java
@@ -3,11 +3,12 @@ public class Queen implements Piece{
     private int yPos;
     private final String color;
 
+
     /**
-     *
-     * @param xPos the queens position on  a-f scale
-     * @param yPos the queens position on the 1-8 scale
-     * @param color the queens color
+     * This is a constructor to create a Queen object with a specified x-position, y-position, and color
+     * @param xPos The x-position of the Queen on a-f scale
+     * @param yPos the y-position of the Queen on 1-8 scale
+     * @param color the color of the Queen
      */
     public Queen(int xPos, int yPos, String color) {
         this.xPos = xPos;
@@ -15,26 +16,29 @@ public class Queen implements Piece{
         this.color = color;
     }
 
+
     /**
-     *
-     * @return the a-h position of the queen ( in coordinate form )
+     * This method returns the x-position of the Queen on a-f scale
+     * @return the x-position of the Queen on a-f scale
      */
     @Override
     public int getXPos() { return this.xPos; }
 
+
     /**
-     *
-     * @return the 1-8 position of the queen
+     * This method returns the y-position of the Queen on a-f scale
+     * @return the y-position of the Queen on a-f scale
      */
     @Override
     public int getYPos() { return this.yPos; }
 
+
     /**
-     *
-     * @param xPos the a-h position the queen wants to move to
-     * @param yPos the 1-8 position the queen wants to move to
-     * @param board the chessboard
-     * @return if the queen can move to the entered coordinate or not
+     * This method determines if the Queen can move to the specified position on the board
+     * @param xPos the a-h position the Queen wants to move to
+     * @param yPos the 1-8 position the Queen wants to move to
+     * @param board the board where the Queen is
+     * @return true if the Queen can move to the entered coordinate, false otherwise
      */
     @Override
     public boolean canMove(int xPos, int yPos, Board board) {
@@ -44,15 +48,16 @@ public class Queen implements Piece{
                 UniversalMethods.canMoveRook(this.xPos, this.yPos, xPos, yPos, board);
     }
 
+
     /**
-     *
-     * @param xPos the a-h position the queen wants to move to
-     * @param yPos the 1-8 position the queen wants to move to
-     * @param board the chessboard
+     * This method moves the Queen to the specified coordinates on the board
+     * @param xPos the a-h position the Queen wants to move to
+     * @param yPos the 1-8 position the Queen wants to move to
+     * @param board the board where the Queen is
      */
     @Override
     public void move(int xPos, int yPos, Board board){
-        // checks if the coordinate the queen wants to move to has a piece there
+        // checks if coordinate the queen wants to move to has a piece there
         // checks if the queen can move to that position
         if(canMove(xPos, yPos, board) && board.getBoard()[yPos-1][xPos-1] == null) {
             board.editBoard(this, this.xPos, this.yPos, xPos, yPos);
@@ -61,14 +66,15 @@ public class Queen implements Piece{
         } else System.out.println("Can't move there");
         // won't throw this exception unless program would crash, unless we use try, catch... but no need for that
         //throw new IllegalArgumentException("Can't move there");
-
     }
 
+
     /**
-     *
-     * @param xPos the a-h position the queen wants to move to and capture
-     * @param yPos the 1-8 position the queen wants to move to and capture
-     * @param board the chessboard
+     * This method checks if the Queen can capture a piece at the specified coordinates on the board
+     * @param xPos the a-h position the Queen wants to move to and capture
+     * @param yPos the 1-8 position the Queen wants to move to and capture
+     * @param board the board where the Queen is
+     * @return true if Queen can capture the given coordinates, false otherwise
      */
     @Override
     public boolean canCapture(int xPos, int yPos, Board board) {
@@ -77,7 +83,6 @@ public class Queen implements Piece{
             return false;
         }
         Piece currPiece = board.getBoard()[yPos-1][xPos-1];
-
         // checks if there's a piece where the queen wants to capture
         // checks if the piece on that square is not the same color as the queen ( cannot capture your own pieces )
         if(canMove(xPos,yPos,board)  && currPiece != null && !currPiece.getColor().equals(this.color)){
@@ -85,31 +90,49 @@ public class Queen implements Piece{
             this.yPos = yPos;
             this.xPos = xPos;
             return true;
-        } else System.out.println("Can't capture there. Piece is not a threat or no piece there.");
+        } else {
+            System.out.println("Can't capture there. Piece is not a threat or no piece there.");
+        }
         //throw new IllegalArgumentException("Can't capture there");
         return false;
     }
 
+
     /**
-     *
+     * This method determines if this Queen is attacking a given piece on the given chessboard
      * @param piece the piece we might be attacking
      * @param board the chessboard
-     * @return if this(the queen) is attacking that piece or not
+     * @return if we're attacking that piece or not
      */
     @Override
-    public boolean isAttacking(Piece piece, Board board) {return this.canMove(piece.getXPos(), piece.getYPos(), board);}
+    public boolean isAttacking(Piece piece, Board board) {
+        return this.canMove(piece.getXPos(), piece.getYPos(), board);
+    }
+
 
     /**
-     *
-     * @return the color of the queen "Black" or "White"
+     * This method returns the color of the Queen
+     * @return the color of the Queen "Black" or "White"
      */
     @Override
-    public String getColor() { return this.color; }
+    public String getColor() {
+        return this.color;
+    }
+
 
     /**
-     *
-     * @return a unique display of the queen with its color and its position
+     * This method returns a unique display of this Queen, including its color
+     * and position on the board
+     * @return a unique display of the Queen with its color and its position
      */
     @Override
-    public String toString(){ return UniversalMethods.changeColor(this.color) + "Queen " + UniversalMethods.changeCord(xPos) + yPos + "\u001B[0m"; }
+    public String toString() {
+        return UniversalMethods.changeColor(this.color) +
+                "Queen " +
+                UniversalMethods.changeCord(xPos) + yPos + "\u001B[0m";
+    }
+
+
 }
+
+
