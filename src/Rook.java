bd91748c941399bbfa4eeb09c460534126b59278
Ahmed Haj Ahmed <ahajahmed@haverford.edu>
2023-05-03 00:00:39 -0400

    added comments to Rook class

--- src/Rook.java
+++ src/Rook.java
@@ -3,11 +3,12 @@ public class Rook implements Piece {
     private int yPos;
     private final String color;
 
+
     /**
-     *
-     * @param xPos the rooks position on  a-f scale
-     * @param yPos the rooks position on the 1-8 scale
-     * @param color the rooks color
+     * This is a constructor to create a Rook object with a specified x-position, y-position, and color
+     * @param xPos The x-position of the Rook on a-f scale
+     * @param yPos the y-position of the Rook on 1-8 scale
+     * @param color the color of the Rook
      */
     Rook(int xPos, int yPos, String color){
         this.color = color;
@@ -15,27 +16,29 @@ public class Rook implements Piece {
         this.yPos = yPos;
     }
 
+
     /**
-     *
-     * @return the a-h position of the rook ( in coordinate form )
+     * This method returns the x-position of the Rook on a-f scale
+     * @return the x-position of the Rook on a-f scale
      */
     @Override
     public int getXPos() { return this.xPos; }
 
+
     /**
-     *
-     * @return the 1-8 position of the rook
+     * This method returns the y-position of the Rook on a-f scale
+     * @return the y-position of the Rook on a-f scale
      */
     @Override
     public int getYPos() { return this.yPos; }
 
 
     /**
-     *
-     * @param xPos the a-h position the rook wants to move to
-     * @param yPos the 1-8 position the rook wants to move to
-     * @param board the chessboard
-     * @return if the rook can move to the entered coordinate or not
+     * This method determines if the Rook can move to the specified position on the board
+     * @param xPos the a-h position the Rook wants to move to
+     * @param yPos the 1-8 position the Rook wants to move to
+     * @param board the board where the Rook is
+     * @return true if the Rook can move to the entered coordinate, false otherwise
      */
     @Override
     public boolean canMove(int xPos, int yPos, Board board) {
@@ -45,30 +48,33 @@ public class Rook implements Piece {
 
 
     /**
-     *
-     * @param xPos the a-h position the rook wants to move to
-     * @param yPos the 1-8 position the rook wants to move to
-     * @param board the chessboard
+     * This method moves the Rook to the specified coordinates on the board
+     * @param xPos the a-h position the Rook wants to move to
+     * @param yPos the 1-8 position the Rook wants to move to
+     * @param board the board where the Rook is
      */
     @Override
     public void move(int xPos, int yPos, Board board){
-        // checks if the coordinate the rook wants to move to has a piece there
+        // checks if coordinate the rook wants to move to has a piece there
         // checks if the rook can move to that position
         if(canMove(xPos, yPos, board) && board.getBoard()[yPos-1][xPos-1] == null) {
             board.editBoard(this, this.xPos, this.yPos, xPos, yPos);
             this.yPos = yPos;
             this.xPos = xPos;
-        } else System.out.println("Can't move there");
+        } else {
+            System.out.println("Can't move there");
+        }
         // won't throw this exception unless program would crash, unless we use try, catch... but no need for that
         //throw new IllegalArgumentException("Can't move there");
-
     }
 
+
     /**
-     *
-     * @param xPos the a-h position the rook wants to move to and capture
-     * @param yPos the 1-8 position the rook wants to move to and capture
-     * @param board the chessboard
+     * This method checks if the Rook can capture a piece at the specified coordinates on the board
+     * @param xPos the a-h position the Rook wants to move to and capture
+     * @param yPos the 1-8 position the Rook wants to move to and capture
+     * @param board the board where the Rook is
+     * @return true if Rook can capture the given coordinates, false otherwise
      */
     @Override
     public boolean canCapture(int xPos, int yPos, Board board) {
@@ -77,7 +83,6 @@ public class Rook implements Piece {
             return false;
         }
         Piece currPiece = board.getBoard()[yPos-1][xPos-1];
-
         // checks if there's a piece where the rook wants to capture
         // checks if the piece on that square is not the same color as the rook ( cannot capture your own pieces )
         if(canMove(xPos,yPos,board)  && currPiece != null && !currPiece.getColor().equals(this.color)){
@@ -90,28 +95,42 @@ public class Rook implements Piece {
         return false;
     }
 
+
     /**
-     *
+     * This method determines if this Rook is attacking a given piece on the given chessboard
      * @param piece the piece we might be attacking
      * @param board the chessboard
-     * @return if this(the rook) is attacking that piece or not
+     * @return if we're attacking that piece or not
      */
     @Override
     public boolean isAttacking(Piece piece, Board board) {
         return this.canMove(piece.getXPos(), piece.getYPos(), board);
     }
 
-    /**
-     *
-     * @return the color of the rook "Black" or "White"
-     */
-    @Override
-    public String getColor() { return this.color; }
 
     /**
-     *
-     * @return a unique display of the rook with its color and its position
+     * This method returns the color of the Rook
+     * @return the color of the Rook "Black" or "White"
      */
     @Override
-    public String toString(){ return UniversalMethods.changeColor(this.color) + " Rook " + UniversalMethods.changeCord(xPos) + yPos + "\u001B[0m"; }
+    public String getColor() {
+        return this.color;
+    }
+
+
+    /**
+     * This method returns a unique display of this Rook, including its color
+     * and position on the board
+     * @return a unique display of the Rook with its color and its position
+     */
+    @Override
+    public String toString() {
+        return UniversalMethods.changeColor(this.color) +
+                " Rook " +
+                UniversalMethods.changeCord(xPos) + yPos + "\u001B[0m";
+    }
+
+
 }
+
+
